786a6f27565c990346e03301ff2804fb2daf93ac
marcel <marcel@fifthrowtech.com>
2026-04-19 16:58:16 +0100

    feat(nvim-012): improve isolated setup

--- scripts/connectAirPods.sh
+++ scripts/connectAirPods.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-bluetoothctl disconnect E4:90:FD:9B:AD:CE & 
+# old id: E4:90:FD:9B:AD:CE
+id="58:64:C4:E7:F1:B0"
+
+bluetoothctl disconnect $id & 
 sleep 1
-bluetoothctl connect E4:90:FD:9B:AD:CE & 
+bluetoothctl connect $id & 
--- scripts/find.sh
+++ scripts/find.sh
@@ -4,12 +4,16 @@
 
 # cd $(find ~/GIT/tek2 -mindepth 1 -maxdepth 2 -type d | grep "B-" | fzf -e )
 
-directories=$(find ~/git -mindepth 1 -maxdepth 5 -name .git -type d | sed 's|\.git||g')
+#directories=$(find ~/git -mindepth 1 -maxdepth 5 -name .git -type d | sed 's|\.git||g')
 #directories=$(find ~/git -mindepth 1 -maxdepth 5 -name .git -type d | replace '.git' '')
 
-cd $(echo "$directories" | fzf)
+
+#set directories $(find ~/git -mindepth 1 -maxdepth 5 -name .git -type d | sed 's|\.git||g')
+#cd $(string split ' ' $directories | fzf)
+#echo $(echo $directories | tr ' ' '\n')
+#cd $(echo -e "$directories" | fzf)
 
 #echo "$directories"
 
 
-#cd $(find ~/GIT/ -mindepth 1 -maxdepth 3 -type d | fzf -e )
+cd $(find ~/git/ -mindepth 1 -maxdepth 3 -type d | fzf -e )
